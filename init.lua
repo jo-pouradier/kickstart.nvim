45ef295992651ff2fdc9618dbf6db0b5fbb16658
jo-pouradier <jocharbo43@gmail.com>
2024-06-04 23:00:23 +0200

    better shortcuts
    enable all gitsigns shortcut.
    use <C-w> for splits control instead of leader v and leader h

--- init.lua
+++ init.lua
@@ -94,7 +94,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -178,12 +178,13 @@ vim.keymap.set('n', '<leader>e', ':NvimTreeOpen<Enter>', { desc = 'Open file [E]
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>se', vim.diagnostic.open_float, { desc = '[S]how diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>de', vim.diagnostic.open_float, { desc = '[D]iagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist, { desc = '[D]iagnostic [Q]uickfix list' })
 
 -- Spliting windows
-vim.keymap.set('n', '<leader>v', '<C-W>v', { desc = '[V]ertical split' })
-vim.keymap.set('n', '<leader>h', '<C-W>s', { desc = '[H]orizontal split' })
+-- vim.keymap.set('n', '<leader>v', '<C-W>v', { desc = '[V]ertical split' })
+-- vim.keymap.set('n', '<leader>h', '<C-W>s', { desc = '[H]orizontal split' })
+-- Just use <C-w>v and <C-w>s
 vim.keymap.set('n', '<leader>x', ':bd<Enter>', { desc = 'e[X]it buffer' })
 
 -- Easy Comment
@@ -280,18 +281,18 @@ require('lazy').setup({
   --    require('gitsigns').setup({ ... })
   --
   -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    config = function()
-      require('gitsigns').setup {
-        preview_config = {
-          border = 'rounded',
-          relative = 'cursor',
-        },
-      }
-      vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<Enter>', { desc = '[G]it [P]review hunk (double to focus)' })
-    end,
-  },
+  -- { -- Adds git related signs to the gutter, as well as utilities for managing changes
+  --   'lewis6991/gitsigns.nvim',
+  --   config = function()
+  --     require('gitsigns').setup {
+  --       preview_config = {
+  --         border = 'rounded',
+  --         relative = 'cursor',
+  --       },
+  --     }
+  --     vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<Enter>', { desc = '[G]it [P]review hunk (double to focus)' })
+  --   end,
+  -- },
   {
     'kdheepak/lazygit.nvim',
     cmd = {
@@ -335,7 +336,7 @@ require('lazy').setup({
       -- Document existing key chains
       require('which-key').register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = '[D]iagnostics', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
@@ -945,7 +946,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
